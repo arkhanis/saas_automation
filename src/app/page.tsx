0a6c659dc61fa53f8586533911dacd6a0f052ee7
arkhanis <casanovac@gmail.com>
2024-04-29 23:47:47 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import Navbar from "@/components/global/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar/>
+    </main>
+  );
+}
